11a183debcd5e253b22259968d51457cdcce5825
nicolasmartello <martellonicolas@gmail.com>
2021-12-27 15:23:22 -0300

    change colors

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   padding: 16px;
   box-shadow: 0 10px 15px -3px rgb(0 0 0 / 10%), 0 4px 6px -2px rgb(0 0 0 / 5%);
-  background-color: #FFF;
+  background-color: var(--light);
   border-radius: 6px;
   display: flex;
   justify-content: flex-start;
